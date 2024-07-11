48487e9c58ac7d4bd9aa8f3f5d0ac17739f1bed3
Jackqueline <mjackqueline22@gmail.com>
2024-07-10 19:22:35 -0700

    added a space

---
+++ app.js
@@ -0,0 +1,26 @@
+// function showRating(rating) {
+//     let ratings = "";
+//     for (let i = 0; i < Math.floor(rating); ++i) {
+//         ratings +=  "*";
+//         if (i !== Math.floor(rating) - 1) {
+//             ratings += " ";
+//         }        
+//     }
+//     if (!Number.isInteger(rating)) {
+//         ratings = ratings + " .";
+//     }
+//     return ratings;
+// }
+
+// console.log(showRating(3));
+// console.log(showRating(4.5));
+// console.log(showRating(0.5));
+
+function showRating(rating) {
+
+}
+
+console.log(showRating(3));
+console.log(showRating(4.5));
+console.log(showRating(0.5));
+
